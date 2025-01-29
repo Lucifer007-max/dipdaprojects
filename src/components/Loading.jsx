f8625910df33691cc30a08487241257763a37aef
lucifer-091 <work.ajay713347@gmail.com>
2025-01-30 04:25:57 +0530

    bg color fix

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,18 +5,26 @@ quantum.register();
 
 const Loading = () => {
   return (
-    <motion.div 
-      className="loader"
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
-      animate={{ rotate: 360 }}
+    <motion.div
+      className="loader bg-custom"
+      initial={{ x: "100vw", opacity: 1 }}  // Start from right
+      animate={{ x: 0, opacity: 1 }}  // Slide in to center
+      exit={{ x: "-100vw", opacity: 0 }}  // Slide out to the left on exit
+      transition={{
+        duration: 1, // Smooth transition duration
+        ease: "easeIn", // Easing for a natural effect
+        delay: 0.3 // Small delay before animation starts
+      }}
     >
-      <l-quantum 
+    <img src="https://www.geospectratech.com/images/logo.png" width={300} />
+      {/* <l-quantum
         size="300"
-        speed="1.75" 
-        color="white" 
-      ></l-quantum>
+        speed="1.75"
+        color="white"
+      ></l-quantum> */}
     </motion.div>
+
+
   );
 };
 
